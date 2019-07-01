bca52648ffe9458c1e218bbca692ce33bfd4e502
DragonSky2357 <qkrdydals327@gmail.com>
2019-07-01 17:25:43 +0900

    CardGGang v1.0

---
+++ CardGGang/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace CardGGang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Pack = new System.Windows.Forms.PictureBox();
+            this.CardCountList = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TotalPowder = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCardOpenCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(633, 119);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(215, 344);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1015, 45);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(215, 344);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(817, 589);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(215, 344);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(1198, 589);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(215, 344);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(1370, 119);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(215, 344);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("굴림", 25F);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Location = new System.Drawing.Point(1072, 482);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 34);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "확인";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Pack
+            // 
+            this.Pack.BackColor = System.Drawing.Color.Transparent;
+            this.Pack.Image = global::CardGGang.Properties.Resources.Pack;
+            this.Pack.Location = new System.Drawing.Point(323, 90);
+            this.Pack.Name = "Pack";
+            this.Pack.Size = new System.Drawing.Size(164, 280);
+            this.Pack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pack.TabIndex = 6;
+            this.Pack.TabStop = false;
+            // 
+            // CardCountList
+            // 
+            this.CardCountList.BackColor = System.Drawing.SystemColors.Control;
+            this.CardCountList.ForeColor = System.Drawing.Color.White;
+            this.CardCountList.FormattingEnabled = true;
+            this.CardCountList.ItemHeight = 12;
+            this.CardCountList.Location = new System.Drawing.Point(259, 589);
+            this.CardCountList.Name = "CardCountList";
+            this.CardCountList.Size = new System.Drawing.Size(282, 352);
+            this.CardCountList.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("굴림", 15F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label2.Location = new System.Drawing.Point(257, 965);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "총 가루 : ";
+            // 
+            // TotalPowder
+            // 
+            this.TotalPowder.AutoSize = true;
+            this.TotalPowder.BackColor = System.Drawing.Color.Transparent;
+            this.TotalPowder.Font = new System.Drawing.Font("굴림", 15F);
+            this.TotalPowder.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.TotalPowder.Location = new System.Drawing.Point(360, 965);
+            this.TotalPowder.Name = "TotalPowder";
+            this.TotalPowder.Size = new System.Drawing.Size(0, 20);
+            this.TotalPowder.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("굴림", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label3.Location = new System.Drawing.Point(257, 998);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "카드깡 수:";
+            // 
+            // lblCardOpenCount
+            // 
+            this.lblCardOpenCount.AutoSize = true;
+            this.lblCardOpenCount.BackColor = System.Drawing.Color.Transparent;
+            this.lblCardOpenCount.Font = new System.Drawing.Font("굴림", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblCardOpenCount.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblCardOpenCount.Location = new System.Drawing.Point(366, 998);
+            this.lblCardOpenCount.Name = "lblCardOpenCount";
+            this.lblCardOpenCount.Size = new System.Drawing.Size(103, 20);
+            this.lblCardOpenCount.TabIndex = 11;
+            this.lblCardOpenCount.Text = "카드깡 수:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CardGGang.Properties.Resources.MainScreen;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.lblCardOpenCount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TotalPowder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CardCountList);
+            this.Controls.Add(this.Pack);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pack)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox Pack;
+        private System.Windows.Forms.ListBox CardCountList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label TotalPowder;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCardOpenCount;
+    }
+}
+
---
+++ CardGGang/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace CardGGang
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer MainMusic = new SoundPlayer(Application.StartupPath + @"\Music\" + "MainMusic.wav");
+        static string defaultPath = Application.StartupPath + @"\CardImage\";
+        string cardPath = defaultPath;
+        PictureBox[] pictureBoxs = new PictureBox[5];
+        const int NormalProbability = 0;
+        const int RareProbability = 70;
+        const int EpicProbability = 95;
+        const int LegendProbability = 99;
+        bool[] checkCard = new bool[5];
+        int[] checkProbability = new int[5];
+        int[] cardCount = new int[4];
+        const int WaitTime = 200;
+        bool isClick = false;
+        int cardOpenCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            MainMusic.PlayLooping();
+        }
+
+        private void LegendCardPlay()
+        {
+            MainMusic.Stop();
+            SoundPlayer simpleSound = new SoundPlayer(Application.StartupPath + @"\Music\" + "Legend.wav");
+            simpleSound.Play();
+            MainMusic.PlayLooping();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBoxs[0] = pictureBox1;
+            pictureBoxs[1] = pictureBox2;
+            pictureBoxs[2] = pictureBox3;
+            pictureBoxs[3] = pictureBox4;
+            pictureBoxs[4] = pictureBox5;
+
+            for (int i = 0; i < pictureBoxs.Length; i++)
+                pictureBoxs[i].BackColor = Color.Transparent;
+            label1.Hide();
+
+            startPoint = Pack.Location;
+        }
+        private int RandJobCardChoice()
+        {
+            Random rand = new Random();
+            return rand.Next(0, 10);
+        }
+        private void CardJobChoice(int choiceValue)
+        {
+            switch (choiceValue)
+            {
+                case 0:
+                    cardPath += @"Normal\";
+                    break;
+                case 1:
+                    cardPath += @"Druid\";
+                    break;
+                case 2:
+                    cardPath += @"Hunter\";
+                    break;
+                case 3:
+                    cardPath += @"Mage\";
+                    break;
+                case 4:
+                    cardPath += @"Paladin\";
+                    break;
+                case 5:
+                    cardPath += @"Priest\";
+                    break;
+                case 6:
+                    cardPath += @"Rogue\";
+                    break;
+                case 7:
+                    cardPath += @"Shaman\";
+                    break;
+                case 8:
+                    cardPath += @"Warlock\";
+                    break;
+                case 9:
+                    cardPath += @"Warrior\";
+                    break;
+            }
+        }
+
+        private int RandomGradeCardChoice()
+        {
+            Random rand = new Random();
+            return rand.Next(0, 100);
+        }
+
+        private void CardGradeChoice(int choiceValue)
+        {
+            if (choiceValue >= LegendProbability)
+            {
+                cardPath += @"Legend\";
+                LegendCardPlay();
+                cardCount[3]++;
+                this.CardCountList.Items.Add("전설카드");
+                CardCountList.SelectedIndex = CardCountList.Items.Count - 1;
+            }
+            else if (choiceValue >= EpicProbability && choiceValue < LegendProbability)
+            {
+                cardPath += @"Epic\";
+                cardCount[2]++;
+                this.CardCountList.Items.Add("특급카드");
+                CardCountList.SelectedIndex = CardCountList.Items.Count - 1;
+            }
+            else if (choiceValue >= RareProbability && choiceValue < EpicProbability)
+            {
+                cardPath += @"Rare\";
+                cardCount[1]++;
+                this.CardCountList.Items.Add("희귀카드");
+                CardCountList.SelectedIndex = CardCountList.Items.Count - 1;
+            }
+            else
+            {
+                cardPath += @"Normal\";
+                cardCount[0]++;
+                this.CardCountList.Items.Add("일반카드");
+                CardCountList.SelectedIndex = CardCountList.Items.Count - 1;
+            }
+        }
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            if (checkCard[0] == true) return;
+            checkCard[0] = true;
+
+            CardJobChoice(RandJobCardChoice());
+            checkProbability[0] = RandomGradeCardChoice();
+
+            bool chk = false;
+            for(int i = 0; i < checkProbability.Length; i++)
+            {
+                if (i == 0) continue;
+                if (checkCard[i] == true)
+                {
+                    chk = true;
+                    break;
+                }
+            }
+
+            if (chk == false)
+                CardGradeChoice(RareProbability);
+            else
+                CardGradeChoice(checkProbability[0]);
+
+            System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(cardPath);
+            System.IO.FileInfo[] fileList = directory.GetFiles("*.png");
+
+            Thread.Sleep(WaitTime);
+
+            Random rand = new Random();
+            int randomValue = rand.Next(0, fileList.Length);
+
+            pictureBox1.Load(cardPath + fileList[randomValue]);
+            cardPath = defaultPath;
+            CheckAllCard();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (checkCard[1] == true) return;
+            checkCard[1] = true;
+
+            CardJobChoice(RandJobCardChoice());
+            checkProbability[1] = RandomGradeCardChoice();
+
+            bool chk = false;
+            for (int i = 0; i < checkProbability.Length; i++)
+            {
+                if (i == 1) continue;
+                if (checkCard[i] == true)
+                {
+                    chk = true;
+                    break;
+                }
+            }
+
+            if (chk == false)
+                CardGradeChoice(RareProbability);
+            else
+                CardGradeChoice(checkProbability[1]);
+
+            System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(cardPath);
+            System.IO.FileInfo[] fileList = directory.GetFiles("*.png");
+
+            Thread.Sleep(WaitTime);
+
+            Random rand = new Random();
+            int randomValue = rand.Next(0, fileList.Length);
+
+            pictureBox2.Load(cardPath + fileList[randomValue]);
+            cardPath = defaultPath;
+            CheckAllCard();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if (checkCard[2] == true) return;
+            checkCard[2] = true;
+
+            CardJobChoice(RandJobCardChoice());
+            checkProbability[2] = RandomGradeCardChoice();
+
+            bool chk = false;
+            for (int i = 0; i < checkProbability.Length; i++)
+            {
+                if (i == 2) continue;
+                if (checkCard[i] == true)
+                {
+                    chk = true;
+                    break;
+                }
+            }
+
+            if (chk == false)
+                CardGradeChoice(RareProbability);
+            else
+                CardGradeChoice(checkProbability[2]);
+
+            System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(cardPath);
+            System.IO.FileInfo[] fileList = directory.GetFiles("*.png");
+
+            Thread.Sleep(WaitTime);
+
+            Random rand = new Random();
+            int randomValue = rand.Next(0, fileList.Length);
+
+            pictureBox3.Load(cardPath + fileList[randomValue]);
+            cardPath = defaultPath;
+            CheckAllCard();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            if (checkCard[3] == true) return;
+            checkCard[3] = true;
+
+            CardJobChoice(RandJobCardChoice());
+            checkProbability[0] = RandomGradeCardChoice();
+
+            bool chk = false;
+            for (int i = 0; i < checkProbability.Length; i++)
+            {
+                if (i == 3) continue;
+                if (checkCard[i] == true)
+                {
+                    chk = true;
+                    break;
+                }
+            }
+
+            if (chk == false)
+                CardGradeChoice(RareProbability);
+            else
+                CardGradeChoice(checkProbability[3]);
+
+            System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(cardPath);
+            System.IO.FileInfo[] fileList = directory.GetFiles("*.png");
+
+            Thread.Sleep(WaitTime);
+
+            Random rand = new Random();
+            int randomValue = rand.Next(0, fileList.Length);
+
+            pictureBox4.Load(cardPath + fileList[randomValue]);
+            cardPath = defaultPath;
+            CheckAllCard();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            if (checkCard[4] == true) return;
+            checkCard[4] = true;
+
+            CardJobChoice(RandJobCardChoice());
+            checkProbability[4] = RandomGradeCardChoice();
+
+            bool chk = false;
+            for (int i = 0; i < checkProbability.Length; i++)
+            {
+                if (i == 4) continue;
+                if (checkCard[i] == true)
+                {
+                    chk = true;
+                    break;
+                }
+            }
+
+            if (chk == false)
+                CardGradeChoice(RareProbability);
+            else
+                CardGradeChoice(checkProbability[4]);
+
+            System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(cardPath);
+            System.IO.FileInfo[] fileList = directory.GetFiles("*.png");
+
+            Thread.Sleep(WaitTime);
+
+            Random rand = new Random();
+            int randomValue = rand.Next(0, fileList.Length);
+
+            pictureBox5.Load(cardPath + fileList[randomValue]);
+            cardPath = defaultPath;
+            CheckAllCard();
+        }
+
+        private void CheckAllCard()
+        {
+            for (int i = 0; i < checkCard.Length; i++)
+                if (checkCard[i] == false) return;
+
+            label1.Show();
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < pictureBoxs.Length; i++)
+                pictureBoxs[i].Load(Application.StartupPath + @"\CardImage\" + "Card.png");
+
+            for (int i = 0; i < checkCard.Length; i++) checkCard[i] = false;
+            label1.Hide();
+
+            this.CardCountList.Items.Add("일반 : " + cardCount[0] + ' '+ "희귀 : " + cardCount[1] + ' '+ 
+                "특급 : " + cardCount[2] + ' '+ "전설 : " + cardCount[3] + '\n');
+            TotalPowder.Text = (cardCount[0] * 5 + cardCount[1] * 20 + cardCount[2] * 100 + cardCount[3] * 400).ToString();
+            lblCardOpenCount.Text = (++cardOpenCount).ToString();
+        }
+
+    }
+}
